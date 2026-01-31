bf6e87b0bc2f004b4ddb823aa3df02adbb474aaf
dirko95 <deianamirko@gmail.com>
2026-01-31 16:51:06 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,12 @@
+
+// Lista prodotti e prezzi
+const PRODUCTS = [
+  { id: "posa_muffola", name: "Posa muffola", price: 158.28 },
+  { id: "posa_pte_esterno", name: "Posa PTE esterno", price: 65.21 },
+  { id: "posa_pte_interno", name: "Posa PTE interno", price: 80.00 },
+  { id: "giunzione", name: "Giunzione", price: 5.61 },
+  { id: "attestazione", name: "Attestazione", price: 41.60 },
+  { id: "terminazione", name: "Terminazione", price: 6.96 },
+  { id: "posa_rfid", name: "Posa RFID", price: 1.55 },
+  { id: "maggiorazione_pte_spillato", name: "Maggiorazione PTE spillato", price: 46.25 }
+];
